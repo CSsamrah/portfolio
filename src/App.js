9cbb76bf109359a9f01f79677f56e875b0f00d3a
CSSamrah <samra.fatima.790@gmail.com>
2024-05-04 22:02:57 +0500

    code updation

--- src/App.js
+++ src/App.js
@@ -9,6 +9,16 @@ import { Box } from '@mui/material';
 
 
 function App() {
+  function downloadResume() {
+    // Replace 'resume.pdf' with the path to your resume file
+    const resumeUrl = './frontend_resume';
+    const link = document.createElement('a');
+    link.href = resumeUrl;
+    link.download = 'Samrah_Fatima_Resume.pdf'; // Set the desired file name
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
   return (
     <div className="App">
       <ResponsiveAppBar />
@@ -16,7 +26,8 @@ function App() {
         <div class="profileinfo">
           <h3>Hi,<br></br> I'm SAMRAH FATIMA ,<br></br>Front-End Developer</h3>
           <p>I design and develop experiences that make people’s lives simpler<br></br> through Web and Mobile apps.I work with Figma , HTML5, CSS3, JavaScript,<br></br> React, ReactNative and Flutter.</p><br></br>
-          <div><button class='mainbutton'>Hire me</button> <button class='mainbutton'>See my projects</button></div>
+          <div><button class='mainbutton' onClick={downloadResume}>Download Resume</button>   
+          <a href="#projects">  <button class='mainbutton'>See my projects</button></a></div>
         </div>
         <BoxSystemProps />
       </div>
